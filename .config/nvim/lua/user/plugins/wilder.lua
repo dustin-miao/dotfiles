b81ac77193938f701ad08e1dc4dc82905f3b7e00
dustin-miao <dutin20.meow@gmail.com>
2024-01-16 13:08:13 -0800

    edited nvim wilder config files

--- .config/nvim/lua/user/plugins/wilder.lua
+++ .config/nvim/lua/user/plugins/wilder.lua
@@ -5,5 +5,27 @@ if not status_ok then
 end
 
 wilder.setup{
-  modes = {'.', '/', '?'}
+  modes = {'.', '/', '?', ':'},
 }
+
+wilder.set_option('renderer', wilder.renderer_mux({
+  [':'] = wilder.popupmenu_renderer({
+    highlighter = wilder.basic_highlighter(),
+    left = {' ', wilder.popupmenu_devicons()},
+    right = {' ', wilder.popupmenu_scrollbar()},
+    pumblend = 30,
+
+    -- wilder.popupmenu_border_theme({
+    --   highlights = {
+    --     border = 'Normal', 
+    --   },
+      -- border = 'rounded',
+    -- }),
+  }),
+  ['/'] = wilder.wildmenu_renderer({
+    highlighter = wilder.basic_highlighter(),
+    separator = ' · ',
+    left = {' ', wilder.wildmenu_spinner(), ' '},
+    right = {' ', wilder.wildmenu_index()},
+  }),
+}))
