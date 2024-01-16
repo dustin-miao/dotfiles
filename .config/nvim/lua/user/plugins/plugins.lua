de2bd109ee70907d1772efe8b958d13edb6ce463
dustin-miao <dutin20.meow@gmail.com>
2024-01-16 09:58:58 -0800

    added obsidian plugin to nvim

--- .config/nvim/lua/user/plugins/plugins.lua
+++ .config/nvim/lua/user/plugins/plugins.lua
@@ -85,6 +85,33 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
 
+  use({
+    "epwalsh/obsidian.nvim",
+    tag = "*",  -- recommended, use latest release instead of latest commit
+    requires = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "personal",
+            path = "~/vaults/personal",
+          },
+          {
+            name = "work",
+            path = "~/vaults/work",
+          },
+        },
+
+        -- see below for full list of options 👇
+      })
+    end,
+  })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
