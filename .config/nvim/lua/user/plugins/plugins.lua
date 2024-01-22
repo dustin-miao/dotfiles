e3044e9b6b8ff8eb4513393c0e52694288a33c27
dustin-miao <dutin20.meow@gmail.com>
2024-01-22 14:51:50 -0800

    added null-ls to nvim

--- .config/nvim/lua/user/plugins/plugins.lua
+++ .config/nvim/lua/user/plugins/plugins.lua
@@ -99,33 +99,6 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
 
-  use({
-    "epwalsh/obsidian.nvim",
-    tag = "*",  -- recommended, use latest release instead of latest commit
-    requires = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-
-      -- see below for full list of optional dependencies 👇
-    },
-    config = function()
-      require("obsidian").setup({
-        workspaces = {
-          {
-            name = "personal",
-            path = "~/vaults/personal",
-          },
-          {
-            name = "work",
-            path = "~/vaults/work",
-          },
-        },
-
-        -- see below for full list of options 👇
-      })
-    end,
-  })
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
