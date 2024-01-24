5377263536a3e99a75b9761db759017c672662b8
dustin-miao <dutin20.meow@gmail.com>
2024-01-23 21:57:32 -0800

    minor modifications to nvim null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -10,11 +10,11 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    diagnostics.checkstyle.with({ extra_args = { "-c", "/scripts/java_checkstyle.xml" } }),
-	},
+  debug = false,
+  sources = {
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    diagnostics.checkstyle.with({ extra_args = { "-c", "~/scripts/java_checkstyle.xml" } }),
+  },
 })
